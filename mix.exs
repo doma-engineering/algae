c3fe57beb889e8429b5287eb8e1be85f7b72fd65
Quinn Wilton <quinn@quinnwilton.com>
2022-01-28 21:53:34 -0800

    Add test/support/* to elixirc_paths
    This causes the test fixtures to be built at compile-time, and
    eliminates a deprecation warning in the call to Code.load_file/1

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule Algae.Mixfile do
       # Versions
       version: "1.3.1-doma",
       elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
 
       # Docs
       name: "Algae",
@@ -31,6 +32,9 @@ defmodule Algae.Mixfile do
       ]
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
